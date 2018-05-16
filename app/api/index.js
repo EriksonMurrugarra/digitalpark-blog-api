d473f784ef8d8448fe39c01be06b356e3813c963
Erikson Murrugarra <erikson.murrugarra@gmail.com>
2018-05-16 01:35:36 -0500

    adding get post

--- app/api/index.js
+++ app/api/index.js
@@ -3,8 +3,10 @@ const router = new express.Router();
 // apis
 const listPosts = require('./list-posts');
 const createPost = require('./create-post');
+const getPost = require('./get-post');
 
 router.get('/v1/posts', listPosts);
 router.post('/v1/posts', createPost);
+router.get('/v1/posts/:key', getPost);
 
 module.exports = router;
