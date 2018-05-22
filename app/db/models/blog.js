469063fe7b281f521a2dd985b2649e5b2574c952
Erikson Murrugarra <erikson.murrugarra@gmail.com>
2018-05-21 23:55:32 -0500

    adding changes to topics

--- app/db/models/blog.js
+++ app/db/models/blog.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const getTopics = tags => tags.join(',');
+const setTopics = tags => tags.split(',');
+
 const postSchema = new Schema({
   key: { type: String, required: true, unique: true },
   title: { type: String, required: true },
   description: { type: String, required: true },
   content: { type: String, required: true },
-  topics: [{ type: String }],
+  topics: { type: [], get: getTopics, set: setTopics },
 }, { timestamps: true });
 
 module.exports = mongoose.model('blog', postSchema);
